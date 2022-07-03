82754d2d59138fb1eb2cab9365feaeab8a354820
Danny2003 <zywzywzyw021@163.com>
2022-07-03 19:20:21 +0800

    blue2white finished

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 mod color;
 use color::write_color;
+mod ray;
 mod vec3;
+pub use ray::Ray;
 use std::{fs::File, io::Write};
 pub use vec3::Vec3;
+fn ray_color(r: &Ray) -> Vec3 {
+    let unit_direction = Vec3::unit(&r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
+}
 fn main() {
-    let file_name = "output/basic_ppm.ppm";
+    let file_name = "output/blue2white.ppm";
     let mut file = File::create(file_name).unwrap();
-    // blue2white.ppm
-    let image_width: i32 = 256;
-    let image_height: i32 = 256;
+
+    // Image
+
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio).floor() as i32;
+
+    // Camera
+
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Vec3::zero();
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    // Render
+
     file.write_all(b"P3\n").expect("wrong write");
     file.write_all(b" ").expect("wrong write");
     file.write_all(image_width.to_string().as_bytes())
@@ -20,11 +45,13 @@ fn main() {
 
     for j in (0..image_height).rev() {
         for i in 0..image_width {
-            let pixel_color = Vec3::new(
-                i as f64 / (image_width - 1) as f64,
-                j as f64 / (image_height - 1) as f64,
-                0.25,
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + horizontal * u + vertical * v - origin,
             );
+            let pixel_color = ray_color(&r);
             write_color(pixel_color, &mut file);
         }
     }
---
+++ src/ray.rs
@@ -0,0 +1,31 @@
+use crate::vec3::Vec3;
+
+#[derive(Clone, Debug, Copy, PartialEq)]
+pub struct Ray {
+    pub orig: Vec3,
+    pub dir: Vec3,
+}
+
+impl Ray {
+    pub fn origin(&self) -> Vec3 {
+        self.orig
+    }
+    pub fn direction(&self) -> Vec3 {
+        self.dir
+    }
+    pub fn new(origin: Vec3, direction: Vec3) -> Self {
+        Self {
+            orig: origin,
+            dir: direction,
+        }
+    }
+    pub fn zero() -> Self {
+        Self {
+            orig: Vec3::zero(),
+            dir: Vec3::zero(),
+        }
+    }
+    pub fn at(&self, t: f64) -> Vec3 {
+        self.orig + self.dir * t
+    }
+}
