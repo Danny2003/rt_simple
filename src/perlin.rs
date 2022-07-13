dc8b8ad3520fd55a32a7d982d5470072ea497c01
Danny2003 <zywzywzyw021@163.com>
2022-07-14 02:05:15 +0800

    Perlin texture, shifted off integer values finished

--- src/perlin.rs
+++ src/perlin.rs
@@ -10,19 +10,19 @@ static POINT_COUNT: usize = 256;
 /// ![Image 6: Tiled random patterns](https://raytracing.github.io/images/img-2.06-tile-random.jpg)
 /// Let’s just use some sort of hashing to scramble this, instead of tiling. This has a bit of support code to make it all happen:
 pub struct Perlin {
-    ran_float: Vec<f64>,
+    ran_vec: Vec<Vec3>,
     perm_x: Vec<i32>,
     perm_y: Vec<i32>,
     perm_z: Vec<i32>,
 }
 impl Perlin {
     pub fn new() -> Self {
-        let mut ran_float = vec![0.; POINT_COUNT];
-        for item in ran_float.iter_mut().take(POINT_COUNT) {
-            *item = random_double();
+        let mut ran_vec = vec![Vec3::zero(); POINT_COUNT];
+        for item in ran_vec.iter_mut().take(POINT_COUNT) {
+            *item = Vec3::unit(Vec3::random_in_range(-1., 1.));
         }
         Self {
-            ran_float,
+            ran_vec,
             perm_x: Self::perlin_generate_perm(),
             perm_y: Self::perlin_generate_perm(),
             perm_z: Self::perlin_generate_perm(),
@@ -38,28 +38,25 @@ impl Perlin {
         // let k = ((p.z() * 4.) as i32 & 255) as usize;
         // // 按位异或
         // self.ran_float[(self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]) as usize]
-        let mut u = p.x() - p.x().floor();
-        let mut v = p.y() - p.y().floor();
-        let mut w = p.z() - p.z().floor();
-        u = u * u * (3. - 2. * u);
-        v = v * v * (3. - 2. * v);
-        w = w * w * (3. - 2. * w);
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
         let i = p.x().floor() as i32;
         let j = p.y().floor() as i32;
         let k = p.z().floor() as i32;
-        let mut c = vec![[[0.; 2]; 2]; 2];
+        let mut c = vec![[[Vec3::zero(); 2]; 2]; 2];
         #[allow(clippy::needless_range_loop)]
         for di in 0..2 {
             for dj in 0..2 {
                 for dk in 0..2 {
-                    c[di][dj][dk] = self.ran_float[(self.perm_x[((i + di as i32) & 255) as usize]
+                    c[di][dj][dk] = self.ran_vec[(self.perm_x[((i + di as i32) & 255) as usize]
                         ^ self.perm_y[((j + dj as i32) & 255) as usize]
                         ^ self.perm_z[((k + dk as i32) & 255) as usize])
                         as usize];
                 }
             }
         }
-        Self::trilinear_interp(c, u, v, w)
+        Self::perlin_interp(c, u, v, w)
     }
     fn perlin_generate_perm() -> Vec<i32> {
         let mut p: Vec<i32> = vec![0; POINT_COUNT];
@@ -76,6 +73,7 @@ impl Perlin {
             p.swap(i, target);
         }
     }
+    #[allow(dead_code)]
     fn trilinear_interp(c: Vec<[[f64; 2]; 2]>, u: f64, v: f64, w: f64) -> f64 {
         let mut accum = 0.;
         #[allow(clippy::needless_range_loop)]
@@ -86,6 +84,25 @@ impl Perlin {
                         * (j as f64 * v + (1. - j as f64) * (1. - v as f64))
                         * (k as f64 * w as f64 + (1. - k as f64) * (1. - w as f64))
                         * c[i][j][k];
+                }
+            }
+        }
+        accum
+    }
+    fn perlin_interp(c: Vec<[[Vec3; 2]; 2]>, u: f64, v: f64, w: f64) -> f64 {
+        let uu = u * u * (3. - 2. * u);
+        let vv = v * v * (3. - 2. * v);
+        let ww = w * w * (3. - 2. * w);
+        let mut accum = 0.;
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    let weight_v = Vec3::new(u - i as f64, v - j as f64, w - k as f64);
+                    accum += (i as f64 * uu + (1. - i as f64) * (1. - uu))
+                        * (j as f64 * vv + (1. - j as f64) * (1. - vv as f64))
+                        * (k as f64 * ww as f64 + (1. - k as f64) * (1. - ww as f64))
+                        * (c[i][j][k] * weight_v);
                 }
             }
         }
